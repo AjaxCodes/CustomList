40c6d5862b5d080cb9aa3561aea156862aada313
unknown <adamjsteinbacher@gmail.com>
2020-06-22 10:59:48 -0500

    initial commit

---
+++ CustomListProjectGroup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomListProjectGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
